766d9754f2b622a3120fbdd7a0f84cb4003e7f65
Ruslan <ruslanshashok322@gmail.com>
2023-12-03 22:36:21 +0200

    fix. update cloud version in .pom file

--- product-service/target/generated-sources/annotations/com/shashok/mapper/ProductMapperImpl.java
+++ product-service/target/generated-sources/annotations/com/shashok/mapper/ProductMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-03T21:41:27+0200",
+    date = "2023-12-03T22:35:11+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
